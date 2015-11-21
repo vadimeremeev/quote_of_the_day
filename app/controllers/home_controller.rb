e1bbab0de06d9863c20642b21b0ac621850c50d3
Vadim Eremeev <vadim@ensogo.com>
2015-11-21 15:14:12 +0800

    added incoming callback to receive messages from the bot

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
+	skip_before_action :verify_authenticity_token, :only => [:incoming_message]
+
 	def index
 
+	end
+
+	def incoming_message
+		Rails.logger.error ">>>Telegram: #{params}"
+		render :nothing => true, :status => 200
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
+  post 'incomingdatafromtelegram9573' => 'home#incoming_message'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
