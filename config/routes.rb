803cb6e191a000e0e0ba33266c179a0836ca320b
Vadim Eremeev <vadim@ensogo.com>
2015-11-21 15:36:29 +0800

    added get method for incoming http requests

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
-  post 'incomingdatafromtelegram9573' => 'home#incoming_message'
+  match 'incomingdatafromtelegram9573' => 'home#incoming_message', via: [:post, :get]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
