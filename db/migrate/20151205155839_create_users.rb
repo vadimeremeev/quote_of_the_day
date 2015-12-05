44a86ec75fe3e7f4800a44aa537b86dfb172be9c
Vadim Eremeev <vadim@ensogo.com>
2015-12-06 00:00:39 +0800

    added users table

---
+++ db/migrate/20151205155839_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.integer :telegram_user_id
+      t.string :first_name
+      t.string :last_name
+      t.string :username
+      t.timestamps null: false
+    end
+  end
+end
