2821adb57f56244f7d94d92444ab5003ac79abe8
Vadim Eremeev <vadim@ensogo.com>
2015-12-06 00:10:27 +0800

    added callback implementation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,23 @@ class HomeController < ApplicationController
 	end
 
 	def incoming_message
+  	message = Telegram.get_message(params)
+  	if message.present?
+  	  user = User.where(:telegram_user_id => message[:chat][:id]).first_or_create(
+        :telegram_user_id => message[:chat][:id],
+        :first_name => message[:chat][:first_name],
+        :last_name => message[:chat][:last_name],
+        :username => message[:chat][:username]
+  	  )
+
+  	  if message[:text].try("include?('/start')")
+  	  	reply = 'Welcome to the Awesome Quote of the Day!'
+  	  else
+  	  	reply = Quote.order("RANDOM()").first
+  	  end
+  	  Telegram.send_message(user.telegram_user_id,reply)
+  	end
+
 		Rails.logger.error ">>>Telegram: #{params}"
 		render :nothing => true, :status => 200
 	end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151205131241) do
+ActiveRecord::Schema.define(version: 20151205155839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 20151205131241) do
     t.string   "title"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.integer  "telegram_user_id"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
 end
