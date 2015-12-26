a62f25f0229d87d1e21071e0aa917f3fbef7d9eb
Vadim Eremeev <vadim@ensogo.com>
2015-12-27 00:37:50 +0800

    add user data initialiser

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,30 +1,33 @@
 class HomeController < ApplicationController
 	skip_before_action :verify_authenticity_token, :only => [:incoming_message]
 
-	def index
+  before_filter :initialise_user
 
+	def index
 	end
 
+  def initialise_user
+    @message = Telegram.get_message(params)
+    if @message.present?
+      @user = User.where(:telegram_user_id => @message[:chat][:id]).first_or_create(
+        :telegram_user_id => @message[:chat][:id],
+        :first_name => @message[:chat][:first_name],
+        :last_name => @message[:chat][:last_name],
+        :username => @message[:chat][:username]
+      )
+    end
+  end
+
 	def incoming_message
-  	message = Telegram.get_message(params)
-  	if message.present?
-  	  user = User.where(:telegram_user_id => message[:chat][:id]).first_or_create(
-        :telegram_user_id => message[:chat][:id],
-        :first_name => message[:chat][:first_name],
-        :last_name => message[:chat][:last_name],
-        :username => message[:chat][:username]
-  	  )
+	  if @message[:text].to_s.include?('/start')
+	  	reply = 'Welcome to the Awesome Quote of the Day!'
+    elsif @message[:text].to_s.include?('/motivate', '/m')
+      reply = Quote.motivation_quote
+	  else
+	  	reply = Quote.random_quote
+	  end
 
-  	  if message[:text].to_s.include?('/start')
-  	  	reply = 'Welcome to the Awesome Quote of the Day!'
-      elsif message[:text].to_s.include?('/motivate')
-        reply = Quote.motivation_quote
-  	  else
-  	  	reply = Quote.random_quote
-  	  end
-
-  	  Telegram.send_message(user.telegram_user_id,reply)
-  	end
+	  Telegram.send_message(@user.telegram_user_id,reply)
 
 		render :nothing => true, :status => 200
 	end
